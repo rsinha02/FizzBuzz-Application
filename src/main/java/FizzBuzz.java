0eb5e86feff57547e7ca9629d73c91e950ec0880
rsinha02 <ravibgp02@gmail.com>
2023-03-21 20:17:05 +0530

    Add:FizzBuzz test when user inputs two

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -8,7 +8,10 @@ public class FizzBuzz {
 	{
 		if(integerTobeChecked==1)
 			return ONE;
-
+	    
+		if(integerTobeChecked==2)
+		 return "2";
+		
 		return String.valueOf(integerTobeChecked);
 
 	}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,12 +4,19 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
 	FizzBuzz fizzBuzz= new FizzBuzz();
-	
+
 	@Test
 	public void shouldReturnOneWhenInputIsOne() 
 	{
-	 Assert.assertEquals("1",fizzBuzz.fizzBuzzConvertor(1));
-		
+		Assert.assertEquals("1",fizzBuzz.fizzBuzzConvertor(1));
+
 	}	
-	
+
+	@Test
+	public void shouldReturnTwoWhenInputIsTwo() 
+	{
+		Assert.assertEquals("2",fizzBuzz.fizzBuzzConvertor(2));
+
+	}
+
 }
