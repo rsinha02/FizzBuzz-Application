4e9b9c04c7bb21a357606768213179c50166a529
rsinha02 <ravibgp02@gmail.com>
2023-03-21 20:13:55 +0530

    Refactor:Constant defined for integer one

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,10 +2,12 @@
 public class FizzBuzz {
 
 
+	private static final String ONE = "1";
+
 	public String fizzBuzzConvertor(int integerTobeChecked)
 	{
 		if(integerTobeChecked==1)
-			return "1";
+			return ONE;
 
 		return String.valueOf(integerTobeChecked);
 
